80efe63c81920aaa89140cc888629f737806b6ed
Corey Bruyere <corey@sundae.com>
2022-02-01 21:03:31 -0800

    feat: Update packages and add Graphql queries to GraphCMS

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,38 @@
+import { useLoaderData, json, Link } from "remix";
+import { GraphQLClient, gql } from "graphql-request";
+
+const GetPostsQuery = gql`
+  {
+    posts {
+      title
+    }
+  }
+`;
+
+export let loader = async () => {
+  const graphcms = new GraphQLClient(
+    "https://api-us-west-2.graphcms.com/v2/ckz4ysyab0ig701z58h7j6k9w/master"
+  );
+
+  const { posts } = await graphcms.request(GetPostsQuery);
+
+  return json({ posts });
+};
+
 export default function Index() {
+  let data = useLoaderData();
+
+  console.log(data);
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+    <ul>
+      {data.posts.map(({ title }: any) => (
+        <li key={title}>
+          {/* <Link to={`/products/${slug}`} prefetch="intent"> */}
+          <a>{title}</a>
+          {/* </Link> */}
         </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      ))}
+    </ul>
   );
 }
